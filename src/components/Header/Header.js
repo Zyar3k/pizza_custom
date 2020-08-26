52523c3d0d63ec9387ecdf85b9c3389fb7dff4cb
Arkadiusz Zygan <a.zygan@gmail.com>
2020-08-26 20:52:58 +0200

    Clean CRA, app pattern

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='headerContent'>Pizza Customization</div>
+    </div>
+  )
+}
